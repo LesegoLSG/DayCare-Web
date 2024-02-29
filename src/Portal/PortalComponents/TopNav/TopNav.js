3edeebe714e111fa35994d5014284608018e985d
LesegoLSG <217057620@student.uj.ac.za>
2024-02-29 00:25:36 -0600

    Added a name next to the profile

--- src/Portal/PortalComponents/TopNav/TopNav.js
+++ src/Portal/PortalComponents/TopNav/TopNav.js
@@ -3,7 +3,8 @@ import EmptyUser from '../../../Assets/EmptyUser.png'
 
 const TopNav = () => {
     return (
-        <div className="bg-gray-400 w-full h-[50px] flex justify-end items-center pr-4  ">
+        <div className="bg-gray-400 w-full h-[50px] flex justify-end items-center pr-4  gap-x-2">
+            <h1>Lesego Mhlongo</h1>
             <div className="bg-red-400 w-10 h-10 rounded-full ">
                 <img className="object-cover" src={EmptyUser} />
             </div>
