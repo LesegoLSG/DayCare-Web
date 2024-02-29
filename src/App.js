0f1d474fa82b738d4f98ec2d14152c77c72cc267
LesegoLSG <217057620@student.uj.ac.za>
2024-02-29 00:24:51 -0600

    Added a nested routes to make use og Outlet from react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/SinglePage/HomePage';
 import PortalPage from './Portal/PortalComponents/PortalMainPage/PortalPage';
 import Users from './Portal/PortalComponents/Users/Users';
-import Blog from './Components/Blog/Blog';
+import BlogPortal from './Portal/PortalComponents/Blog/BlogPortal';
 import Dashboard from './Portal/PortalComponents/Dashboard/Dashboard';
 import Authentication from './Components/LoginRegister/Authentication';
 
@@ -13,13 +13,15 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-
-          <Route path="/portal" element={<PortalPage />} />
-          <Route path="/portal/users" element={<Users />} />
-          <Route path="/portal/blog" element={<Blog />} />
-          <Route path="/portal/dashboard" element={<Dashboard />} />
-
           <Route path="/login" element={<Authentication />} />
+
+          <Route path="/portal" element={<PortalPage />} >
+            <Route index element={<Dashboard />} />
+            <Route path="/portal/users" element={<Users />} />
+            <Route path="/portal/blog" element={<BlogPortal />} />
+          </Route>
+
+
 
         </Routes>
       </Router>
