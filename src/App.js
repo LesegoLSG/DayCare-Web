6c2fa51be5c7eb8253f9f61acc0e86dd15fc958f
LesegoLSG <217057620@student.uj.ac.za>
2024-02-28 21:31:48 -0600

    Added more routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/SinglePage/HomePage';
+import PortalPage from './Portal/PortalComponents/PortalMainPage/PortalPage';
+import Users from './Portal/PortalComponents/Users/Users';
+import Blog from './Components/Blog/Blog';
+import Dashboard from './Portal/PortalComponents/Dashboard/Dashboard';
+import Authentication from './Components/LoginRegister/Authentication';
 
 function App() {
   return (
@@ -8,6 +13,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+
+          <Route path="/portal" element={<PortalPage />} />
+          <Route path="/portal/users" element={<Users />} />
+          <Route path="/portal/blog" element={<Blog />} />
+          <Route path="/portal/dashboard" element={<Dashboard />} />
+
+          <Route path="/login" element={<Authentication />} />
 
         </Routes>
       </Router>
