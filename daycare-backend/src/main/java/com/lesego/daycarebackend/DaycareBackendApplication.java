9a6ae992f08d9ad5d482e91c49a612094f076cce
LesegoLSG <217057620@student.uj.ac.za>
2024-02-29 10:09:41 -0600

    Backend code

---
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/DaycareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lesego.daycarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaycareBackendApplication.class, args);
+	}
+
+}
---
+++ daycare-backend/src/test/java/com/lesego/daycarebackend/DaycareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesego.daycarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaycareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
