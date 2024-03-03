8a0be00d8ffbb1169359fac96b10fc637c720f61
LesegoLSG <217057620@student.uj.ac.za>
2024-03-03 01:14:14 -0600

    Added a route for addUser

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Users from './Portal/PortalComponents/Users/Users';
 import BlogPortal from './Portal/PortalComponents/Blog/BlogPortal';
 import Dashboard from './Portal/PortalComponents/Dashboard/Dashboard';
 import Authentication from './Components/LoginRegister/Authentication';
+import AddUser from './Portal/PortalComponents/Users/AddUser';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/portal" element={<PortalPage />} >
             <Route index element={<Dashboard />} />
             <Route path="/portal/users" element={<Users />} />
+            <Route path="/portal/users/add" element={<AddUser />} />
             <Route path="/portal/blog" element={<BlogPortal />} />
           </Route>
 
