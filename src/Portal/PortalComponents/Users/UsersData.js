297e82ca5b531d1d6299883fcb12ebc05633f2c5
LesegoLSG <217057620@student.uj.ac.za>
2024-03-03 01:18:02 -0600

    Json user data for testing

---
+++ src/Portal/PortalComponents/Users/UsersData.js
@@ -0,0 +1,66 @@
+const usersData = [
+    {
+        id: 1,
+        image: "pp",
+        firstname: "Lesego",
+        lastname: "Mhlongo",
+        mobile: "0765528945",
+        email: "lesegomhlogno78@gmail.com",
+        designation: "Office",
+        roles: "MODERATOR"
+    },
+    {
+        id: 2,
+        image: "pp",
+        firstname: "Tshiamiso",
+        lastname: "Mhlongo",
+        mobile: "0765528945",
+        email: "tshiamhlogno78@gmail.com",
+        designation: "Learner",
+        roles: "ADMIN"
+
+    },
+    {
+        id: 3,
+        image: "pp",
+        firstname: "dfdfdfdf",
+        lastname: "Mhlfdfdfongo",
+        mobile: "0765528945",
+        email: "lesegodfdfmhlogno78@gmail.com",
+        designation: "Principal",
+        roles: "CREATOR"
+
+    },
+    {
+        id: 4,
+        image: "pp",
+        firstname: "Ldfdesego",
+        lastname: "Mhlodfdfngo",
+        mobile: "0765528945",
+        email: "lesegomhlogdfdfno78@gmail.com",
+        designation: "Gate keeper",
+        roles: "MODERATOR",
+    },
+    {
+        id: 5,
+        image: "pp",
+        firstname: "Ldfdesego",
+        lastname: "Mhlodfdfngo",
+        mobile: "0765528945",
+        email: "lesegomhlogdfdfno78@gmail.com",
+        designation: "Gate keeper",
+        roles: "ADMIN",
+    },
+    {
+        id: 6,
+        image: "pp",
+        firstname: "Ldfdesego",
+        lastname: "Mhlodfdfngo",
+        mobile: "0765528945",
+        email: "lesegomhlogdfdfno78@gmail.com",
+        designation: "Gate keeper",
+        roles: "CONTENT CREATOR",
+    },
+]
+
+export default usersData;
