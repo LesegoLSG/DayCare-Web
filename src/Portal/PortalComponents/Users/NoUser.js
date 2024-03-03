70a65686d8c408b62a173a50593643fadf77f91c
LesegoLSG <217057620@student.uj.ac.za>
2024-03-03 01:17:24 -0600

    Component to display when there are no user to display or length of users is less than or equals to 0

---
+++ src/Portal/PortalComponents/Users/NoUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoUser = () => {
+    return (
+        <div className="bg-white w-[50%] min-h-[20rem] flex justify-center items-center">
+            <h1 className="text-2xl font-bold">No User's</h1>
+        </div>
+    )
+}
+
+export default NoUser
