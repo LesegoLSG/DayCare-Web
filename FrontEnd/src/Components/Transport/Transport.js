0c5a154adc5d0a9d6329382283cd49e80757aaa6
LesegoLSG <217057620@student.uj.ac.za>
2024-03-08 09:54:23 -0600

    Added google map api

--- FrontEnd/src/Components/Transport/Transport.js
+++ FrontEnd/src/Components/Transport/Transport.js
@@ -1,44 +1,24 @@
 import React, { useState } from 'react';
 import Lottie from "lottie-react";
-import animationData from './Bus.json';
+import animationHome from './AnimationJSONTransport/Home.json';
+import animationDestination from './AnimationJSONTransport/Destination.json';
 import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 
 const Transport = () => {
-    const [isMoved, setIsMoved] = useState(false);
-
-    const handleMoveRight = () => {
-        setIsMoved(true);
-    };
+    const navigate = useNavigate();
+    const address = "1330 Mosia Street Moletsane, Soweto"
+    // Assuming contact.address holds the address string
+    // const formattedAddress = address.replace(/\s/g, '+'); // Replace spaces with '+'
+    // Construct the Google Maps URL
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
 
     return (
         <div className="bg-gray-600 w-full h-screen relative ">
             <h1>Transport section</h1>
-            <motion.div
-                whileInView={{
-                    translateX: "60vw",
-                    // translateY: "500px"
-                    // display: 'none'
-
-                }}
-                // initial={{
-                //     translateX: "0px",
-                //     // translateY: "0px",
-                //     // rotate: "45deg"
-                // }}
-                // animate={{
-                //     translateX: "2000px",
-                //     // translateY: "500px",
-                //     // translateY: "500px"
-                //     // rotate: "45deg"
-                // }}
-                transition={{
-                    duration: 8.5,
-                    delay: 2
-                }}
-
-                className={`bg-red-200 w-40 h-40 ml-4 absolute transition-transform ${isMoved ? 'translate-x-40' : 'translate-x-0'}`}>
-                <Lottie animationData={animationData} />
-            </motion.div>
+            <div className=" w-40 h-40 ml-4 absolute ">
+                <Lottie animationData={animationHome} />
+            </div>
 
             <div className="absolute w-80 h-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <h1>Day Care</h1>
@@ -46,10 +26,18 @@ const Transport = () => {
                     et magnam deserunt ipsam ab in
                     quaerat quidem ex porro neque inventore ea excepturi labore, dolorum
                     debitis incidunt sequi. Eveniet, veritatis.</p>
-                <button className="bg-blue-200 p-4">Read More</button>
+                <button
+                    onClick={() => navigate("/transportService")}
+                    className="bg-blue-200 p-4">Read More</button>
             </div>
             {/* <button className="absolute bottom-2 right-2 bg-blue-500 text-white px-3 py-1 rounded" onClick={handleMoveRight}>Move Right</button> */}
+            <div className="absolute bottom-4 right-4 w-60 h-60">
+                <Lottie animationData={animationDestination} />
+                <a href={googleMapsUrl} target="_blank" >1330 Mosia Street</a>
+            </div>
         </div>
+
+
     )
 }
 
