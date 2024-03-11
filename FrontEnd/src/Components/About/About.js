4a11a3e2ef15833b9452647e8a07e8159a33ae64
LesegoLSG <217057620@student.uj.ac.za>
2024-03-11 00:13:17 -0500

    Added age group and trading hours

--- FrontEnd/src/Components/About/About.js
+++ FrontEnd/src/Components/About/About.js
@@ -21,20 +21,22 @@ const About = () => {
                 </div>
                 <div className="flex flex-col justify-center items-center">
 
-                    <p>We provide a nurturing and playful environment for children to blossom and thrieve
-                        , fostering creativity, curiosity, and compassion in every child, preparing them for a lifetime
-                        of success.
-                    </p>
-                    <div className="flex flex-col justify-center items-start">
-                        <h5 className="m-1 font-bold">What we offer:</h5>
-                        <p><span className="bg-red-500  rounded-full m-1 p-1 text-xl inline-flex justify-center items-center">1</span>Early childhood education</p>
-                        <p><span className="bg-blue-500  rounded-full m-1 p-1 text-xl inline-flex justify-center items-center">2</span>Nutritious Meals</p>
-                        <p><span className="bg-green-500  rounded-full m-1 p-1 text-xl inline-flex justify-center items-center">3</span>Indoors and Outdoors Activities</p>
-                    </div>
-                    <div>
-                        <h5 className="m-1 font-bold">Our Promise:</h5>
-                        <p>Every day is a memorable adventure filled with laughter, joy, and endless possibilities.</p>
-                        <p>Join Us: Let's embark on a journey of discovery together!</p>
+                    <p>We are a trusted daycare center with over 10 years of experience in providing a nurturing and playful environment for children to blossom and thrive. At [Mamoriti], we believe in fostering creativity, curiosity, and compassion in every child, preparing them for a lifetime of success.</p>
+
+                    <div className="bg-red-400 w-full grid grid-cols-2 gap-1 mt-10">
+                        <div className="bg-white w-auto h-auto ">
+                            <h1 className="text-green-600 font-bold transform -rotate-6">AGE GROUP:</h1>
+                            <h2 className="font-bold">2-6 Years</h2>
+                            <h1 className="text-green-600 font-bold transform -rotate-6">Grade R</h1>
+                            <h2 className=" font-bold transform -rotate-10">5-6 Years</h2>
+                        </div>
+                        <div className="bg-blue-200 w-auto h-auto ">
+                            <h1 className="text-green-600 font-bold">TRADING HOURS:</h1>
+                            <h2>7:00am - 4:00pm</h2>
+                            <h2>Monday - Thursday</h2>
+                            <h2>Friday 7:00am - 3:00pm</h2>
+                            <h2>Closed public holidays</h2>
+                        </div>
                     </div>
 
                     <button className="bg-red-200 m-6" >Learn More</button>
