229138d4c31259f9441acbe97a1209236201b9a0
LesegoLSG <217057620@student.uj.ac.za>
2024-03-11 00:25:01 -0500

    Folder containing a component for taking a selfie

---
+++ FrontEnd/src/Portal/PortalComponents/Users/VideoOrImageDisplay/VideoOrImageDisplay.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+
+const VideoOrImageDisplay = ({ image, setImage, showVideo, uploadsection }) => {
+    const [videoLoaded, setVideoLoaded] = useState(false);
+    const [imageUrl, setImageUrl] = useState(null);
+
+    let videoRef = useRef(null);
+    let photoRef = useRef(null);
+
+    //Get access to user webCamera
+
+    const getUserCamera = () => {
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        })
+            .then((stream) => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+
+                video.onloadedmetadata = () => {
+                    setVideoLoaded(true);
+                    video.play().catch(error => console.error(error));
+                };
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    // to take a picture
+    const takePicture = (e) => {
+        e.preventDefault();
+
+        let width = 500
+        let height = width / (16 / 9)
+
+        let photo = photoRef.current;
+        console.log("photo:", photo);
+
+        let video = videoRef.current;
+        console.log("video:", video);
+
+        photo.width = width;
+        photo.height = height;
+
+        let ctx = photo.getContext('2d');
+        ctx.drawImage(video, 0, 0, photo.width, photo.height);
+
+        console.log("photo1:", photo);
+
+        // Convert canvas content to data URL
+        const imageDataURL = photo.toDataURL();
+
+        // Convert data URL to Blob
+        const blob = dataURItoBlob(imageDataURL);
+
+        // Construct a File object
+        const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+
+        // Set the captured image file to the Image state
+        setImage(file);
+        console.log("Capture (take a picture) method:", image);
+
+        setImageUrl(imageDataURL);
+
+    }
+
+    // Function to convert data URI to Blob
+    const dataURItoBlob = (dataURI) => {
+        const byteString = atob(dataURI.split(',')[1]);
+        const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        const ab = new ArrayBuffer(byteString.length);
+        const ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        return new Blob([ab], { type: mimeString });
+    }
+
+
+    const clearImage = () => {
+        let photo = photoRef.current
+        if (photo) {
+            let ctx = photo.getContext('2d');
+            ctx.clearRect(0, 0, photo.width, photo.height)
+        }
+        setImage(null);
+        setVideoLoaded(false); // Reset video loaded state
+        getUserCamera();
+
+    }
+
+    useEffect(() => {
+        getUserCamera()
+    }, [])
+
+    useEffect(() => {
+        if (videoLoaded) {
+            videoRef.current.play().catch(error => console.error(error));
+        }
+    }, [videoLoaded]);
+
+    useEffect(() => {
+        console.log("Image:", image);
+        if (image) {
+            setImageUrl(URL.createObjectURL(image)); // Update imageUrl when image state changes
+        }
+    }, [image]);
+
+
+
+
+    return (
+        <div>
+            <div className="w-80 h-80 border rounded-full">
+                {image ? (
+                    <img
+                        className="w-80 h-80 object-fill border rounded-full"
+                        src={imageUrl} alt="Captured" />
+                ) : (
+                    <video className="w-80 h-80 object-fill border rounded-full" ref={videoRef}></video>
+                )}
+                {!image && <canvas ref={photoRef}></canvas>}
+            </div>
+            {/* Buttons */}
+            {image && showVideo ? (
+                <button onClick={clearImage}>ReCapture</button>
+            ) : (
+                <button
+                    className='bg-red-600'
+                    onClick={(e) => takePicture(e)}>Take a selfie</button>
+            )}
+            <button onClick={uploadsection}>Rather Upload</button>
+        </div>
+    );
+}
+
+export default VideoOrImageDisplay
