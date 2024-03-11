a0c3ae5df871b92b734528ba55bdb5c4521fc8d8
LesegoLSG <217057620@student.uj.ac.za>
2024-03-11 00:30:09 -0500

    Modified a service for adding a user, deleting a user and getting a user list

--- daycare-backend/src/main/java/com/lesego/daycarebackend/Service/UserServices/IUserServiceMethods.java
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/Service/UserServices/IUserServiceMethods.java
@@ -1,7 +1,16 @@
 package com.lesego.daycarebackend.Service.UserServices;
 
+import com.lesego.daycarebackend.Entity.User.User;
+import com.lesego.daycarebackend.dto.UserInformation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IUserServiceMethods {
     public String addUser (MultipartFile image, String userJson);
+
+    public List<UserInformation> getAllUsers();
+
+    public ResponseEntity<String> deleteUser(int id);
 }
--- daycare-backend/src/main/java/com/lesego/daycarebackend/Service/UserServices/UserServiceMethods.java
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/Service/UserServices/UserServiceMethods.java
@@ -5,11 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lesego.daycarebackend.Entity.User.User;
 import com.lesego.daycarebackend.Repository.UserRepo.UserRepository;
 import com.lesego.daycarebackend.Reusables.ImageUtils;
+import com.lesego.daycarebackend.dto.UserInformation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceMethods implements IUserServiceMethods {
@@ -23,15 +29,17 @@ public class UserServiceMethods implements IUserServiceMethods {
         User user;
         try {
             user = objectMapper.readValue(userJson, User.class);
+            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
             System.out.println("user:" + user);
         } catch (JsonProcessingException e) {
             // Handle JSON parsing exception
             throw new IllegalArgumentException("Invalid user JSON format");
         }
 
-        // Set the image data from the multipart file
+        // Set the image data from the multipart file and encode password
         try {
             user.setImage(ImageUtils.compressImage(image.getBytes()));
+
         } catch (IOException e) {
             // Handle file processing exception
             throw new IllegalArgumentException("Error processing image file");
@@ -43,4 +51,51 @@ public class UserServiceMethods implements IUserServiceMethods {
         userRepository.save(user);
         return "User added successfully:"+ user.getFirstName();
     }
+
+    @Override
+    public ResponseEntity<String> deleteUser(int id) {
+        Optional<User> user = userRepository.findById(id);
+       if(user.isPresent()){
+           userRepository.delete(user.get());
+           return ResponseEntity.ok("User with id: " + id + " is deleted successfully");
+       }else{
+           return ResponseEntity.notFound().build();
+       }
+    }
+
+
+    /**
+     *
+     * @return list of users
+     * Method to return all users
+     */
+    @Override
+    public List<UserInformation> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        List<UserInformation> userInformationList = new ArrayList<>();
+
+        System.out.println("userList: " + userList);
+        for(User user : userList){
+            UserInformation userInformation = new UserInformation();
+            userInformation.setId(user.getId());
+            userInformation.setFirstName(user.getFirstName());
+            userInformation.setLastName(user.getLastName());
+            userInformation.setEmail(user.getEmail());
+            userInformation.setMobile(user.getMobile());
+            userInformation.setPassword(user.getPassword());
+            userInformation.setRole(user.getRole());
+            userInformationList.add(userInformation);
+
+            if(user.getImage() == null){
+                userInformation.setImage(null);
+            }else{
+                userInformation.setImage(ImageUtils.decompressImage(user.getImage()));
+            }
+
+        }
+        return userInformationList;
+    }
+
+
+
 }
