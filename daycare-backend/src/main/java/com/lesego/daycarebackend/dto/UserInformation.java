6b8f88e2f8d7a8c1f0c1b1db917f34e98be2d15b
LesegoLSG <217057620@student.uj.ac.za>
2024-03-11 00:26:01 -0500

    dto for user information

---
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/dto/UserInformation.java
@@ -0,0 +1,83 @@
+package com.lesego.daycarebackend.dto;
+
+import com.lesego.daycarebackend.Entity.User.Role;
+import jakarta.persistence.*;
+
+public class UserInformation {
+
+    private int id;
+    private byte[] image;
+    private String firstName;
+    private String lastName;
+
+    private String mobile;
+
+    private String email;
+    private String password;
+
+    private Role role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
