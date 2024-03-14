edb824d4d8bce07b8958a2c51a5ca627beaef85c
LesegoLSG <217057620@student.uj.ac.za>
2024-03-14 09:37:31 -0500

    Added a button for navigating to the add blog section

--- FrontEnd/src/Portal/PortalComponents/Blog/BlogPortal.js
+++ FrontEnd/src/Portal/PortalComponents/Blog/BlogPortal.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { IoMdAdd } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 
 const BlogPortal = () => {
+    const navigate = useNavigate();
     return (
-        <div>Blog page</div>
+        <div className="flex justify-between m-4">
+            <h1>Blog</h1>
+            <button
+                onClick={() => navigate("/portal/blog/add")}
+                className="bg-blue-200 h-8 w-40 flex flex-row justify-center items-center">
+                <IoMdAdd />
+                Create Blog
+            </button>
+        </div>
     )
 }
 
