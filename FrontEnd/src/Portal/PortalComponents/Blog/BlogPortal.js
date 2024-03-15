c49e1b418d552e697b021d5c46b42fdad5350813
LesegoLSG <217057620@student.uj.ac.za>
2024-03-15 01:07:48 -0500

    Added cards by mapping through a list of blogs

--- FrontEnd/src/Portal/PortalComponents/Blog/BlogPortal.js
+++ FrontEnd/src/Portal/PortalComponents/Blog/BlogPortal.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { IoMdAdd } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
+import BlogPortalCard from './BlogPortalCard';
+import blogData from '../../../Components/Blog/BlogData';
+import NoBlog from './NoBlog';
 
 const BlogPortal = () => {
     const navigate = useNavigate();
     return (
-        <div className="flex justify-between m-4">
-            <h1>Blog</h1>
-            <button
-                onClick={() => navigate("/portal/blog/add")}
-                className="bg-blue-200 h-8 w-40 flex flex-row justify-center items-center">
-                <IoMdAdd />
-                Create Blog
-            </button>
-        </div>
+        <section className="main-container  w-full ">
+            <div className="flex justify-between m-4">
+                < h1 className="text-2xl font-semibold"> Blog</h1 >
+                <button
+                    onClick={() => navigate("/portal/blog/add")}
+                    className="bg-blue-200 h-8 w-40 flex flex-row justify-center items-center">
+                    <IoMdAdd />
+                    Create Blog
+                </button>
+            </div >
+
+            <div className="grid grid-cols-4 ">
+                {blogData.length > 0 ? (
+                    blogData.map((myBlogData, index) => (
+                        <BlogPortalCard key={index} singleBlog={myBlogData} />
+                    ))
+                ) : (
+                    <NoBlog />
+                )
+
+                }
+            </div>
+        </section >
     )
 }
 
