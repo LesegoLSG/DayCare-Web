6ce9cb5aa159080296e5a7afad8e89adfa3a7989
LesegoLSG <217057620@student.uj.ac.za>
2024-03-20 15:36:58 -0500

    Testing

--- FrontEnd/src/Components/Contact/Contact.js
+++ FrontEnd/src/Components/Contact/Contact.js
@@ -4,9 +4,33 @@ import Background from '../../Assets/Background.png';
 const Contact = () => {
     return (
         <section id="contact" className="bg-blue-200 w-full h-auto pt-20">
-            <img
+            {/* <img
                 className="bg-cover bg-no-repeat"
-                src={Background} />
+                src={Background} /> */}
+            <div className="w-full h-auto">
+                <div className="w-full md:w-3/4 h-auto md:flex md:flex-wrap">
+                    {/* Left side */}
+                    <div className="w-full md:w-3/4">
+                        <div className="grid md:grid-cols-3 gap-4">
+                            {/* Your blog cards */}
+                            <div className="bg-gray-200 p-4">Blog Card 1</div>
+                            <div className="bg-gray-200 p-4">Blog Card 2</div>
+                            <div className="bg-gray-200 p-4">Blog Card 3</div>
+                            {/* Add more blog cards as needed */}
+                        </div>
+                    </div>
+                    {/* Right side */}
+                    <div className="w-full md:w-1/4 md:ml-4">
+                        <div className="md:flex md:flex-col md:justify-between">
+                            {/* Latest Post */}
+                            <div className="bg-gray-300 p-4 mb-4">Latest Post</div>
+                            {/* Upcoming Events */}
+                            <div className="bg-gray-300 p-4">Upcoming Events</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section>
     )
 }
