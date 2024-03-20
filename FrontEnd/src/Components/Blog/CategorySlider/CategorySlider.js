ef021f88a5d42dc1d35eddafb3bc6f97e77eb300
LesegoLSG <217057620@student.uj.ac.za>
2024-03-20 15:46:58 -0500

    Category filtering

---
+++ FrontEnd/src/Components/Blog/CategorySlider/CategorySlider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CategorySlider = ({ categories, selectedCategory, handleCategoryFilter }) => {
+    return (
+        <div className="category-slider-container overflow-x-auto whitespace-nowrap">
+            {categories.map((category, index) => (
+                <button
+                    key={index}
+                    className={`inline-block px-4 py-2 mr-2 mb-2 rounded-lg ${selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}
+                    onClick={() => handleCategoryFilter(category)}
+                >
+                    {category}
+                </button>
+            ))}
+        </div>
+    )
+}
+
+export default CategorySlider
