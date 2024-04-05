bc5f7b30c71d3acafd4156692a3e099a9692242c
LesegoLSG <217057620@student.uj.ac.za>
2024-04-05 10:15:23 -0500

    Removed the background color and added border

--- FrontEnd/src/Components/Services/Modal/Modal.js
+++ FrontEnd/src/Components/Services/Modal/Modal.js
@@ -14,18 +14,18 @@ const Modal = ({ service, onCloseModal }) => {
             onClick={handleOnClose}
             className="fixed inset-0 z-50 bg-black bg-opacity-30 backdrop:blur-sm flex justify-center items-center">
 
-            <div className="bg-white w-[80%] max-h-[80vh] p-2 rounded">
-                <div className="bg-green-200 w-[100%] flex justify-end">
+            <div className="bg-primary border-secondary border-2 w-[80%] max-h-[80vh] p-2 rounded">
+                <div className=" w-[100%] flex justify-end">
                     {/* <button className="bg-red-600 " onClick={() => onCloseModal()}>Close</button> */}
                     <IoMdCloseCircleOutline
-                        className="text-red-600 text-2xl font-semibold cursor-pointer"
+                        className=" text-2xl font-semibold cursor-pointer"
                         onClick={() => onCloseModal()} />
                 </div>
                 <h1 className="text-2xl font-semibold">{service.title}</h1>
 
-                <div className="bg-orange-600 grid sm:grid-cols-1 md:grid-cols-2 justify-center items-center">
+                <div className=" grid sm:grid-cols-1 md:grid-cols-2 justify-center items-center">
                     {/* Image div */}
-                    <div className=" bg-pink-600 h-[20rem] flex justify-center items-center">
+                    <div className=" h-[20rem] flex justify-center items-center">
                         <img
                             className="object-cover w-full h-full"
                             src={imageMain}
@@ -33,7 +33,7 @@ const Modal = ({ service, onCloseModal }) => {
                     </div>
 
                     {/* Full description div */}
-                    <div className="bg-pink-800 flex justify-center items-center">
+                    <div className=" flex justify-center items-center">
                         <p>{extendedDescription}</p>
                     </div>
 
