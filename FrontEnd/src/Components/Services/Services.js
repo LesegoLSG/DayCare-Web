bb084ef8c4bcf4bfcb72eb9952b2f89d8aa3b571
LesegoLSG <217057620@student.uj.ac.za>
2024-04-05 10:16:47 -0500

    Chaned the background and action button colors

--- FrontEnd/src/Components/Services/Services.js
+++ FrontEnd/src/Components/Services/Services.js
@@ -15,15 +15,15 @@ const Services = () => {
     };
 
     return (
-        <section className="bg-blue-200 w-full h-auto flex flex-col justify-center items-center" id="services">
-            <div className="bg-green-400 w-full h-auto flex justify-start items-center pl-16">
+        <section className="bg-secondary w-full h-auto flex flex-col justify-center items-center" id="services">
+            <div className=" w-full h-auto flex justify-start items-center pl-16">
                 <h3>Our offer</h3>
             </div>
-            <div className="bg-red-400 w-full h-auto flex justify-start items-center pl-16">
+            <div className="w-full h-auto flex justify-start items-center pl-16">
                 <h1 className="text-xl font-bold">Our Best Offer <br></br>For Your Kid</h1>
             </div>
             {/* Main box */}
-            <div className="bg-green-300 w-full h-auto mt-6 grid grid-cols-1 gap-1 md:grid-cols-3 place-items-center px-16">
+            <div className=" w-full h-auto mt-6 grid grid-cols-1 gap-1 md:grid-cols-3 place-items-center px-16">
                 {
                     serviceData.map((myService, index) => (
                         <ServicesCard key={index} singleService={myService} onOpenModal={handleOpenModal} index={index} />
--- FrontEnd/src/Components/Services/ServicesCard.js
+++ FrontEnd/src/Components/Services/ServicesCard.js
@@ -13,13 +13,13 @@ const ServicesCard = ({ singleService, onOpenModal, index }) => {
 
     return (
 
-        <div className="bg-red-200 w-[18rem] h-[18rem] m-6 p-6 flex flex-col justify-center items-center relative rounded-full  border-4  border-solid " style={{ borderColor: colorStyling }}>
-            <div className="bg-red-400 rounded-full w-[5rem] h-[5rem] absolute top-0 right-2 transform -translate-y-1/2 border border-black border-solid">
+        <div className=" w-[18rem] h-[18rem] m-6 p-6 flex flex-col justify-center items-center relative rounded-full  border-4  border-solid " style={{ borderColor: colorStyling }}>
+            <div className="bg-balance rounded-full w-[5rem] h-[5rem] absolute top-0 right-2 transform -translate-y-1/2 border border-black border-solid">
                 {animation}
             </div>
             <h1 className="text-base font-semibold mt-6">{title}</h1>
             <p>{briefDescription}</p>
-            <button className="bg-red-600 mt-4" onClick={() => onOpenModal(singleService)}>Read More</button>
+            <button className=" mt-4 px-1 py-1" onClick={() => onOpenModal(singleService)}>Read More</button>
         </div>
     )
 }
