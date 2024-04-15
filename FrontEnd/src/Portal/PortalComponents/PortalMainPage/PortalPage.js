ce997e0bbae1b9d50e5a48499f1f0aff86089a85
LesegoLSG <217057620@student.uj.ac.za>
2024-04-14 20:58:40 -0500

    Getting the loggedIn user and set the user to userContext

--- FrontEnd/src/Portal/PortalComponents/PortalMainPage/PortalPage.js
+++ FrontEnd/src/Portal/PortalComponents/PortalMainPage/PortalPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SideBar from '../SideBar/SideBar';
 import Dashboard from '../Dashboard/Dashboard';
 import Users from '../Users/Users';
@@ -6,9 +6,38 @@ import BlogPortal from '../Blog/BlogPortal';
 import TopNav from '../TopNav/TopNav';
 
 import { Outlet } from 'react-router-dom';
+import AuthService from '../../../AuthServices/AuthService/AuthService';
+
+import { useUser } from '../../../UserContext/UserLoggedIn';
 
 
 const PortalPage = () => {
+
+    const { setLoggedInUser } = useUser();
+
+    useEffect(() => {
+        console.log("useEffect...")
+        const fetchLoggedInUser = async () => {
+            try {
+                console.log("It reaches the try catch");
+                const userEmail = AuthService.getUserEmail();
+                console.log("Checking email:", userEmail);
+                if (userEmail) {
+                    const response = await AuthService.getCurrentlyLoggedInUser(userEmail);
+                    console.log("currently loggedin user response:", response);
+                    console.log("currently loggedin user response data:", response.data);
+                    if (response) {
+                        setLoggedInUser(response);
+                        // console.log("LoggedIn User:", loggedInUser);
+                    }
+                }
+
+            } catch (error) {
+                console.log("Error in useEffect:", error);
+            }
+        };
+        fetchLoggedInUser();
+    }, []);
 
 
     return (
