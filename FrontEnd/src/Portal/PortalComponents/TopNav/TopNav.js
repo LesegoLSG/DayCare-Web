172ea6ba59ac523e63675cbdf7b418d3b5ff7a13
LesegoLSG <217057620@student.uj.ac.za>
2024-04-14 21:01:00 -0500

    Displaying the logged in user first name,last name and image

--- FrontEnd/src/Portal/PortalComponents/TopNav/TopNav.js
+++ FrontEnd/src/Portal/PortalComponents/TopNav/TopNav.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import EmptyUser from '../../../Assets/EmptyUser.png'
+import EmptyUser from '../../../Assets/EmptyUser.png';
+import { useUser } from '../../../UserContext/UserLoggedIn';
 
 const TopNav = () => {
+    const { loggedInUser, setLoggedInUser } = useUser();
+    const userImage = loggedInUser.image || EmptyUser;
     return (
         <div className="bg-gray-400 w-full h-[50px] flex justify-end items-center pr-4  gap-x-2">
-            <h1>Lesego Mhlongo</h1>
-            <div className="bg-red-400 w-10 h-10 rounded-full ">
-                <img className="object-cover" src={EmptyUser} />
+            <h1>{`${loggedInUser.firstName} ${loggedInUser.lastName}`}</h1>
+            <div className="bg-red-400 w-10 h-10 rounded-full "
+                style={{
+                    backgroundImage: `url('data:image/**;base64,${userImage}')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+
+            >
+
             </div>
         </div>
     )
