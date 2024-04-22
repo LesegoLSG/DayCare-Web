b7950b631141e196c89509938e956b04d5efa987
LesegoLSG <217057620@student.uj.ac.za>
2024-04-21 20:41:38 -0500

    Testing the refresh token

--- FrontEnd/src/Portal/PortalComponents/Dashboard/Dashboard.js
+++ FrontEnd/src/Portal/PortalComponents/Dashboard/Dashboard.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUser } from '../../../UserContext/UserLoggedIn';
+import AuthService from '../../../AuthServices/AuthService/AuthService';
 
 const Dashboard = () => {
     const { loggedInUser } = useUser();
+    const [refreshTokenResponse, setRefreshTokenResponse] = useState(null);
+
+    // useEffect(() => {
+    //     const refreshToken = AuthService.getRefreshToken();
+    //     const refresh = async () => {
+    //         try {
+    //             const response = await AuthService.refreshToken(refreshToken);
+    //             setRefreshTokenResponse(response);
+    //         } catch (error) {
+    //             console.error("Error refreshing token:", error);
+    //             // Handle error if needed
+    //         }
+    //     };
+
+    //     if (refreshToken) {
+    //         refresh();
+    //     }
+    // }, []);
+
+    const handleChecking = async () => {
+        const refreshToken = AuthService.getRefreshToken();
+        try {
+            const response = await AuthService.refreshToken(refreshToken);
+            setRefreshTokenResponse(response.data);
+        } catch (error) {
+            console.error("Error refreshing token:", error);
+            // Handle error if needed
+        }
+    }
+
     return (
         <div>
             Dashboard
             {loggedInUser.firstName}
             {loggedInUser.mobile}
+
+            {refreshTokenResponse && (
+                <p>Refresh Token Response: {JSON.stringify(refreshTokenResponse)}</p>
+            )}
+
+            <button onClick={handleChecking}>Checking</button>
         </div>
     )
 }
