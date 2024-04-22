18b8bad9dfe42ef2c8e27834099a8c35415a8d7a
LesegoLSG <217057620@student.uj.ac.za>
2024-04-21 20:47:42 -0500

    Added method from the admin controller

--- daycare-backend/src/main/java/com/lesego/daycarebackend/Controller/UserController/UserController.java
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/Controller/UserController/UserController.java
@@ -1,11 +1,14 @@
 package com.lesego.daycarebackend.Controller.UserController;
 
 import com.lesego.daycarebackend.Service.UserServices.IUserServiceMethods;
+import com.lesego.daycarebackend.dto.UserInformation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -17,5 +20,44 @@ public class UserController {
         return ResponseEntity.ok("Hi User");
     }
 
+    @Autowired
+    private IUserServiceMethods userService;
 
+    @PostMapping("/add")
+    public ResponseEntity<String> addNewUser(@RequestParam("image") MultipartFile image, @RequestParam("user") String userJson){
+        userService.addUser(image,userJson);
+        return ResponseEntity.ok("User added successfully.");
+    }
+
+    @GetMapping("/getData")
+    public String getData(){
+        return "It works";
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<UserInformation>> getUsers(){
+        List<UserInformation> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id){
+        return userService.deleteUser(id);
+    }
+
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable int id,@RequestParam("image") MultipartFile image, @RequestParam("user") String userJson){
+        return userService.updateUser(id,image,userJson);
+    }
+    @GetMapping("/getRoleFromEmail/{email}")
+    public String getUserRoleFromEmail(@PathVariable String email) {
+        System.out.println(email);
+        String role = userService.getRoleFromUserEmail(email);
+        System.out.println("Role: " + role);
+        if (!role.isEmpty()) {
+            return role;
+        } else {
+            return "Error return user";
+        }
+    }
 }
