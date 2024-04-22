83c91bd6ec557f0c353ee967289f241c9d18848e
LesegoLSG <217057620@student.uj.ac.za>
2024-04-21 20:39:50 -0500

    Testing the refresh token to provide new access toekn

--- FrontEnd/src/AuthServices/Axios/AxiosPrivateInstance.js
+++ FrontEnd/src/AuthServices/Axios/AxiosPrivateInstance.js
@@ -1,61 +1,53 @@
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import AuthService from '../AuthService/AuthService';
 
 
-
+//Base url
 const axiosPrivateInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
-
+//Attaches an end point with a bear token, but validating expiration time for a token and refreshes the token before a request is sent to the server
 axiosPrivateInstance.interceptors.request.use(
     async (config) => {
+        // const navigate = useNavigate();
         const token = AuthService.getToken();
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
+            const tokenExpiration = AuthService.getTokenExpiration();
+            console.log("TokenExpiration: ", tokenExpiration);
+            const currentTime = new Date().getTime();
+            if (tokenExpiration && tokenExpiration < currentTime) {
+                //Token expired, attempting to refresh the token
+                try {
+                    const refreshToken = AuthService.getRefreshToken();
+                    if (refreshToken) {
+                        const response = await AuthService.refreshToken(refreshToken);
+                        if (response?.data?.token) {
+                            // Set a new Token and attach the token to the request
+                            AuthService.setToken(response.data.token);
+                            config.headers.Authorization = `Bearer ${response.data.token}`;
+                        }
+                    } else {
+                        // No refresh token available, redirect to login page
+                        <Navigate to="/login" />
+                    }
+
+                } catch (error) {
+                    AuthService.clearTokens();
+                    <Navigate to="/login" />
+                }
+            } else {
+                // Token is still valid, attach it to the request
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+
         }
         return config;
     },
     (error) => {
         return Promise.reject(error);
     }
-);
-
-axiosPrivateInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        const navigate = useNavigate();
-        const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
-            console.log("401 status reached");
-            originalRequest._retry = true;
-            const refreshToken = AuthService.getRefreshToken();
-            if (refreshToken) {
-                try {
-                    const response = await AuthService.refreshToken(refreshToken);
-                    if (response?.data?.token) {
-
-                        console.log("New refreshToken:", response.data.token);
-
-                        AuthService.setToken(response.data.token);
-                        originalRequest.headers['Authorization'] = `Bearer ${response.data.token}`;
-                        return axiosPrivateInstance(originalRequest);
-                    }
-                } catch (refreshError) {
-                    console.log("Refresh Token Error:", refreshError);
-                    AuthService.clearTokens(); // Clear tokens if refresh fails
-                    navigate("/login");
-                }
-            } else {
-                AuthService.clearTokens(); // Clear tokens if no refresh token is found
-                navigate("/login");
-            }
-        }
-        return Promise.reject(error);
-    }
-
 );
 
 
