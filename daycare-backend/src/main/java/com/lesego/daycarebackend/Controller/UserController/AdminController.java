f94a35d973c69852af40caf2478a46cc521818a8
LesegoLSG <217057620@student.uj.ac.za>
2024-04-21 20:47:05 -0500

    Removed all methods

--- daycare-backend/src/main/java/com/lesego/daycarebackend/Controller/UserController/AdminController.java
+++ daycare-backend/src/main/java/com/lesego/daycarebackend/Controller/UserController/AdminController.java
@@ -23,46 +23,7 @@ public class AdminController {
         return ResponseEntity.ok("Hi Admin");
     }
 
-    @Autowired
-    private IUserServiceMethods userService;
 
-    @PostMapping("/add")
-    public ResponseEntity<String> addNewUser(@RequestParam("image") MultipartFile image, @RequestParam("user") String userJson){
-        userService.addUser(image,userJson);
-        return ResponseEntity.ok("User added successfully.");
-    }
-
-    @GetMapping("/getData")
-    public String getData(){
-        return "It works";
-    }
-
-    @GetMapping("/getAllUsers")
-    public ResponseEntity<List<UserInformation>> getUsers(){
-       List<UserInformation> users = userService.getAllUsers();
-       return ResponseEntity.ok(users);
-    }
-
-    @DeleteMapping("/deleteUser/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable int id){
-        return userService.deleteUser(id);
-    }
-
-    @PutMapping("/updateUser/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable int id,@RequestParam("image") MultipartFile image, @RequestParam("user") String userJson){
-        return userService.updateUser(id,image,userJson);
-    }
-    @GetMapping("/getRoleFromEmail/{email}")
-    public String getUserRoleFromEmail(@PathVariable String email) {
-        System.out.println(email);
-        String role = userService.getRoleFromUserEmail(email);
-        System.out.println("Role: " + role);
-        if (!role.isEmpty()) {
-            return role;
-        } else {
-            return "Error return user";
-        }
-    }
 //    @GetMapping("/currentlyLoggedInUser/{email}")
 //    public UserInformation getCurrentLoggedInUser(@PathVariable String email){
 //        return userService.getLoggedInUserInfo(email);
