22475035a5bf577dd3609d6cf6d6b41425de7775
LesegoLSG <lesegomhlongo78@gmail.com>
2024-06-05 00:18:04 -0500

    Modified the looks

--- FrontEnd/src/Components/LoginRegister/Login.jsx
+++ FrontEnd/src/Components/LoginRegister/Login.jsx
@@ -3,12 +3,17 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import AuthService from '../../AuthServices/AuthService/AuthService';
 import { IoIosEyeOff } from "react-icons/io";
 import { IoEye } from "react-icons/io5";
+import LoginPlayingKids from '../../Assets/LoginPlayingKids.jpg';
 
 const Login = () => {
     const navigate = useNavigate();
     const [passwordVisibility, setPasswordVisibility] = useState(false);
 
     const handleVisibility = () => {
+        setPasswordVisibility(!passwordVisibility);
+    }
+
+    const togglePasswordVisibility = () => {
         setPasswordVisibility(!passwordVisibility);
     }
 
@@ -41,47 +46,69 @@ const Login = () => {
     };
 
     return (
-        <div className="bg-white w-[80%] h-[30rem] md:w-[30%] md:h-[26rem] rounded-lg shadow-lg">
-            <form onSubmit={handleLoginSubmit}>
-                <h1 className="text-xl font-semibold m-6">Login</h1>
-                <div className="flex flex-col justify-center items-start p-2">
-                    <label className="">Email:</label>
-                    <input
-                        className="w-full h-8 border rounded-md"
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        value={loginDetails.email}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="flex flex-col justify-center items-start p-2 relative"> {/* Add relative positioning */}
-                    <label className="">Password:</label>
-                    <div className="w-full flex flex-row">
-                        <input
-                            className="w-full h-8 border rounded-md pr-8"
-                            type={passwordVisibility ? "text" : "password"}
-                            placeholder="Password"
-                            name="password"
-                            value={loginDetails.password}
+        <section className="w-full h-screen flex justify-center items-center">
+            {/* Main container */}
+            <div className="w-full h-screen flex justify-center items-center">
+                {/* left div */}
+                <div className='w-full md:w-1/2 h-screen flex flex-col justify-center items-center bg-white'>
+                    <h1 className="h1 my-6">Welcome Back</h1>
+                    <h2 className="h2 ">Mamoriti Daycare Center</h2>
+                  
+                    <form className="mt-4 w-full flex flex-col justify-center items-center gap-y-6 " onSubmit={handleLoginSubmit}>
+                        <div className="w-4/5 flex flex-col justify-start items-center ">
+                            <label className="w-4/5 label text-start">Email Address</label>
+                            <input 
+                            type="email"
+                            name="email"
+                            value={loginDetails.email}
                             onChange={handleInputChange}
-                        />
-                        <div className="absolute right-4 top-5 bottom-0 flex justify-center items-center"> {/* Position the eye icon absolutely */}
-                            <div className="rounded-l-lg" onClick={handleVisibility}>
-                                {!passwordVisibility ? (
-                                    <IoEye size={25} />
-                                ) : (
-                                    <IoIosEyeOff size={25} />
-                                )}
-                            </div>
+                             className="w-4/5 inputField" 
+                             />
                         </div>
+                <div className="w-4/5 flex flex-col justify-center items-center p-2 relative"> {/* Add relative positioning */}
+                    <label className="w-4/5 label text-start">Password:</label>
+                    <div className="relative w-4/5">
+                                <input
+                                    type={passwordVisibility ? "text" : "password"}
+                                    className="w-full inputField"
+                                    name="password"
+                                    value={loginDetails.password}
+                                    onChange={handleInputChange}
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute right-2 top-1/2 transform -translate-y-1/2 focus:outline-none"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    {passwordVisibility ? (
+                                        <IoEye className="text-gray-500" size={20} />
+                                    ) : (
+                                        <IoIosEyeOff className="text-gray-500" size={20} />
+                                    )}
+                                </button>
+                            </div>
+                </div>
+                        <div className="w-4/5 flex flex-col justify-end items-center ">
+                            <p className="w-4/5 text-end cursor-pointer">Forgot Password?</p>
+                        </div>
+                        <div className="w-4/5 flex flex-col justify-end items-center  mt-6">
+                            <button type="submit" className="button">Login</button>
+                        </div>
+                    </form>
+                </div>
+                {/* Right */}
+                <div className='hidden md:flex w-1/2 h-screen  justify-start items-center bg-blue-200 relative'>
+                    <div className=''>
+                        <img src={LoginPlayingKids} alt="Books" className="h-screen " />
                     </div>
+                    <h1 className="h1 absolute top-50 left-60 text-white">Building Bright Futures</h1>
+
                 </div>
-                <div className="flex flex-col justify-center items-center p-2">
-                    <button className="bg-blue-200 w-full h-8 mt-4 rounded-md" type="submit">Login</button>
-                </div>
-            </form>
-        </div>
+
+            </div>
+
+
+        </section>
     )
 }
 
